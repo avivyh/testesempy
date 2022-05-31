d9b21104969e2e36359e5f8c02d949d0a6a3a70f
avivyh <viviannyemilia14@gmail.com>
2022-05-31 09:19:43 -0300

    new

---
+++ 06.py
@@ -0,0 +1,12 @@
+#  Created by avivyh on 31/05/22.
+#  Copyright © 2022  avivyh. All rights reserved.
+
+"""
+    Em python, tuplas são muito semelhantes a listas. A principal diferença é
+    que tuplas são imutáveis, ou seja, não podem ser alteradas.
+
+    tuplas são construidas com o uso de parenteses e virgula.
+    ex: 
+    tupla = (item1, item2, item3)
+    
+"""
