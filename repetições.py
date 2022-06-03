48c587709c102066d5ac8724c1bf04549aaf0d0c
avivyh <viviannyemilia14@gmail.com>
2022-06-03 15:38:10 -0300

    new

--- repetições.py
+++ repetições.py
@@ -50,3 +50,72 @@ for i in range(0, 500, 100):
 #imprimindo caracter por caracter de uma frase
 for i in 'PYTHON É MUITO BOM!!!!!!':
     print(i)
+print("")
+
+#loops aninhados 
+for i in range(0, 5):
+    for a in range(0, 5):
+        print(a)
+    print('')
+
+#usando operações em uma lista dentro de um for
+Lista = [12, 12, 14, 15, 16, 17, 18, 19]
+soma = 0
+for i in Lista:
+    double = i * 2
+    soma += double
+    print(soma)
+print('')
+
+#percorrendo lista de lista
+Lista = [[1, 2, 3, 4], [6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16]]
+for valor in Lista:
+    print(valor)
+print('')
+
+#contando a quantidade de itens de uma lista
+Lista = [12, 12, 14, 15, 16, 17, 18, 19]
+cont = 0
+for i in Lista:
+    cont+= 1
+print(cont)
+print("")
+
+
+#contando o número de colunas
+Lista = [[1, 2, 3, 4], [6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16]]
+cont = 0
+primeiraL = Lista[0]
+for i in primeiraL:
+    cont += 1
+print(cont)
+print("\n")
+
+
+#pesquisando em listas
+Lista = [12, 12, 14, 15, 16, 17, 18, 19]
+for i in Lista:
+    if (i == 18):
+        print('Valor encontrado na lista!\n')
+
+
+#mostrando as chaves de um dicionario
+dic = {'k1':'Python', 'k2':'C', 'k3':'C++'}
+for i in dic:
+    print(i)
+print('\n')
+
+#printando a chave e o valor de um dicionário
+for c,v in dic.items():
+    print(c, v)
+print('\n')
+
+
+'''
+While em py
+
+while(exp1):
+    print('comando contiua sendo executado enquanto a exp1 for verdadeira')
+
+'''
+
