214e8760bc4ae84b2fe3b7cb30121bb50159f42a
avivyh <viviannyemilia14@gmail.com>
2022-06-08 11:36:10 -0300

    new

---
+++ laboratório02.py
@@ -0,0 +1,28 @@
+#imprimindo o menu para o usuário
+print('*********************************** PYTHON CALCULADORA ***********************************\n')
+print('Selecione o número da operação desejada:\n')
+print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n 4 - Divisão\n')
+#solicitando a operação a ser realizada
+operação = int(input('Digite o número correspondente a operação: (1/2/3/4)\n'))
+print('')
+#solicitando valores
+x = int(input('Digite o primeiro número:\n'))
+y = int(input('Digite o segundo número:\n'))
+print('')
+#opções de operações
+#chamando a função anonima lambda para solucionar as operações e imprimindo o resultado
+if(operação == 1):
+    result = lambda x, y: x+y
+    print(x, '+', y, '=', result(x, y))
+elif(operação == 2):
+    result = lambda x, y: x-y
+    print(x, '-' , y, '=', result(x, y))
+elif(operação == 3):
+    result = lambda x, y: x*y
+    print(x, '*' , y, '=', result(x, y))
+elif(operação == 4):
+    result = lambda x, y: x/y
+    print(x, '/' , y, '=', result(x, y))
+else:
+    print('Operação não reconhecida!')
+print('')
