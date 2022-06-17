dda3267787e182346af3398d1d42ab85c4d74657
avivyh <viviannyemilia14@gmail.com>
2022-06-17 08:22:19 -0300

    new

---
+++ datetime.py
@@ -0,0 +1,7 @@
+#  Created by avivyh on 17/06/22.
+#  Copyright © 2022  avivyh. All rights reserved.
+
+import datetime
+#passando a hora atual p variavel agora, usando o método datetime do módulo date time 
+agora = datetime.datetime.now()
+print(agora)
