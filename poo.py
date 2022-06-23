ca8789d70ee1220347bab77c7bb72823a5e62df4
avivyh <viviannyemilia14@gmail.com>
2022-06-22 21:37:59 -0300

    new

---
+++ poo.py
@@ -0,0 +1,51 @@
+'''
+
+DEFINIÇÃO DE ORIENTAÇÃO A OBJETOS
+PYTHON É UMA LINGUAGEM ORIENTADA A OBJETOS.
+A ORIENTAÇÃO A OBJETOS É UM MODELO DE ANALISE, PROJETO E PROGRAMAÇÃO DE SISTEMA DE SOFTWARE
+BASEADO NA COMPOSIÇÃO E INTERAÇÃO ENTRE DIVERSAS  UNIDADES DE SOFTWARE CHAMADAS DE OBJETO.
+
+
+A PROGRAMAÇÃO ORIENTADA A OBJETOS FOI CRIADA PARA TENTAR APROXIMAR O MUNDO REAL E O VIRTUAL.
+A IDEIA FUNDAMENTAL É TENTAR SIMULAR O MUNDO REAL DENTRO DO COMPUTADOR.
+
+
+EM POO, O PROGRAMADOR É RESPONSÁVEL POR MOLDAR O MUNDO DOS OBJETOS E DEFINIR COMO OS OBJETOS DEVEM INTERAGIR
+ENTRE SI.
+
+OS OBJETOS 'CONVERSAM' UNS COM OS OUTROS ATRAVÉS DO ENVIO DE MENSAGENS E O PAPEL PRINCIPAL DO PROGRAMADOR É DEFINIR
+QUAIS SERÃO AS MENSAGENS QUE CADA OBJETO PODE RECEBER E TAMBÉM QUAL A AÇÃO QUE O OBJETO DEVE REALIZAR AO RECEBER CADA MENSAGEM.
+
+
+#PROGRAMAÇÃO ESTRUTURADA: SEQUÊNCIA, DECISÃO, ITERAÇÃO (REPETIÇÃO)
+
+
+#PROGRAMAÇÃO ORIENTADA A OBJETOS: MÉTODOS SÃO FUNÇÕES, DENTRO DE CLASSES. QUE TEM COMO OBJETIVO MANIPULAR OS ATRIBUTOS
+DO OBJETO.
+
+UMA CLASSE POSSUI ATRIBUTOS OU PROPRIEDADES E POSSUI METODOS.
+ATRIBUTOS SÃO CARACTERISTICAS, POR EX: na classe CACHORRO, tamanho e raça seriam atributos. latir seria um método. E um
+método nada mais é, do que uma função.
+
+
+'''
+
+#---------------------------------------------------------------------------------------------------------------------------------
+''' 
+
+PRINCIPAIS CONCEITOS DE PROGRAMAÇÃO ORIENTADA A OBJETOS 
+
+CLASSE          - classes são abstrações computacionais que representam entidades do mundo real  
+OBJETO          - objeto é uma instancia de uma classe, possui atributos que são caracteristicas ou propriedades do objeto.
+ATRIBUTOS       - são caracteristicas ou propriedades que ajudam a identificar os objetos
+MÉTODOS         - métodos são ações do objeto, são funções dentro de uma classe
+MENSAGEM        - mensagem é uma chamada ao objeto para invocar unm dos seus métodos
+HERANÇA         - é o mecanismo pelo qual uma class(subc) pode herdar outra classe(superc)aproveitando seus atributos e metodos.
+POLIMORFISMO    - as classes derivadas de uma unica classe base são capazes de invocar os métodos que embora apresentem a mesma definição, comportam-se de maneira diferente para cada uma das classes derivadas.
+                        (os mesmos atributos e metodos podem ser usados em objetos distintos, porém com implementações lógicas diferentes.)
+ENCAPSULAMENTO  - é a tecnica que faz com que detalhes internos do funcionamento dos metodos de uma classe permaneçam ocultos para os objetos.
+
+
+
+
+'''
