769adaff9730853099afca3282a61ca0d732d02f
avivyh <viviannyemilia14@gmail.com>
2022-08-02 19:33:29 -0300

    new

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
 #para criar uma classe, utiliza-se a palavra reservada class 
 # o nome da sua classe segue a mesma convenção de nomes de funções e variaveis 
 # mas geralmente se usa a primeira letra da palavra maiuscula no nome da classe
+
